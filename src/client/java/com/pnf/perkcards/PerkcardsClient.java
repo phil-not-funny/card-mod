9ddde8e97b9e8c0157cdbd68cc8d4923d2ec2cfd
philnotfunny <schrenk.philip@hotmail.com>
2024-12-07 15:32:21 +0100

    refactor packages

---
+++ src/client/java/com/pnf/perkcards/PerkcardsClient.java
@@ -0,0 +1,10 @@
+package com.pnf.perkcards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PerkcardsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/pnf/perkcards/PerkcardsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pnf.perkcards;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PerkcardsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
